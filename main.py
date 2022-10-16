42c1eab0e18722cd50d9aa9e9d5279f2f7c258ee
oohlearn <learnooh@gmail.com>
2022-10-16 16:48:00 +0800

    add filesand update about catch errors

---
+++ main.py
@@ -0,0 +1,38 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+# print(phonetic_dict)
+
+# Solution 1
+effective = True
+
+while effective:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        effective = False
+        print(output_list)
+
+# Solution 2
+
+
+def generate_phonetic():
+    word_ = input("Enter a word: ").upper()
+    try:
+        output_list_ = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
